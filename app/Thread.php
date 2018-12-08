1b8c2453dc72a882ab3d6e2ea6b1e466b90f88d1
Jhonny Martinez <jhonnymartinez@live.com>
2018-12-08 12:42:56 -0400

    Thread has many replies

--- app/Thread.php
+++ app/Thread.php
@@ -7,12 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     
-        public function path() {
+    public function path() {
 
-            return '/threads/' . $this->id;
+        return '/threads/' . $this->id;
 
-        }
+    }
 
+    public function replies() {
+
+        return $this->hasMany(Reply::class);
+
+    }
 
 
 }
