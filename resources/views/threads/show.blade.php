dc6d4b0ddf2b1abe138fef153a63a1783066dd79
Jhonny Martinez <jhonnymartinez@live.com>
2018-12-09 12:21:27 -0400

    Replies added to the thread

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -12,5 +12,20 @@
             </div>
         </div>
     </div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Replies especiales</div>
+                @foreach ($thread->replies as $reply)
+                    <div class="card-body">
+                        {{ $reply->body }} <br>
+                        <a href="#">{{ $reply->owner->name }} </a> said {{ $reply->created_at->diffForHumans() }}
+                    </div>
+                    <hr>
+                @endforeach
+            </div>
+
+        </div>
+    </div>
 </div>
 @endsection
